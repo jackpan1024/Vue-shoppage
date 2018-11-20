b19680ae26854219fe91044e77acec49c4cb9d8e
jackpan <jackpan7796@qq.com>
2018-11-20 18:48:27 +0800

    完成轮播图和九宫格的基本样式

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
-// console.log(123)
+// 导入vue 
 import Vue from "vue"
-
+// 导入  APP 的组件
 import app from "./App.vue"
+
+//导入接口的请求文件
+import VueResource from "vue-resource"
+
+// 注册接口请求
+Vue.use(VueResource)
+// 阻止vue 启动的时候的默认提示功能
+Vue.config.productionTip = false
 
 // import MintUi from "mint-ui"
 
@@ -10,9 +18,11 @@ import app from "./App.vue"
 // console.log(__dirname)
 
 // 按需导入   在导入的时候是需要先下载 babel-plugin-babelrc 的包 然后再按照官方文档配置相关的文件
-import { Header,Button } from 'mint-ui';
+import { Header,Button,Swipe, SwipeItem } from 'mint-ui';
 Vue.component(Header.name, Header);
 Vue.component(Button.name, Button);
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
 
  // 在同一个项目中是可以同时使用多个框架老构建项目的
  //使用mui 专业的手机移动端布局  因为mui没有npm  所以直接下载包 然后导入包
@@ -23,6 +33,11 @@ import "./lib/mui/css/icons-extra.css"
 
 // 导入路由模板
 import router from "./router/"
+
+
+// 导入公共样式模块
+
+import "./css/com.css"
 
 
 
--- src/page/homePage/index.vue
+++ src/page/homePage/index.vue
@@ -1,10 +1,125 @@
 <template>
-    <h1> 这是首页</h1>
+   <div>
+    <div class="banner">
+       <mt-swipe :auto="4000" class="banner-list">
+            <mt-swipe-item v-for="(item, index) in bannerList" :key="index">
+                <a :href="item.url">
+                    <img :src="item.img" alt="">
+                </a>
+            </mt-swipe-item>
+        </mt-swipe> 
+    </div>
+    <div class="title">
+        <ul class="mui-table-view mui-grid-view mui-grid-9">
+		            <li v-for="(item,index) in title" :key="index" class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+                        <a href="#">
+		                   <img :src="item.url" alt="">
+		                    <p>{{item.name}}</p>
+                        </a>
+                    </li>
+		            <!-- <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-email"><span class="mui-badge">5</span></span>
+		                    <div class="mui-media-body">Email</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-chatbubble"></span>
+		                    <div class="mui-media-body">Chat</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-location"></span>
+		                    <div class="mui-media-body">location</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-search"></span>
+		                    <div class="mui-media-body">Search</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-phone"></span>
+		                    <div class="mui-media-body">Phone</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-gear"></span>
+		                    <div class="mui-media-body">Setting</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-info"></span>
+		                    <div class="mui-media-body">about</div></a></li>
+		           <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <span class="mui-icon mui-icon-more"></span>
+		                    <div class="mui-media-body">more</div></a></li> -->
+		        </ul> 
+    </div>
+   </div>
 </template>
 <script>
-    
+// 插件  提示的功能
+import { Toast } from "mint-ui";
+export default {
+  data() {
+    return {
+      bannerList: [],
+      title:[
+          {name:'新闻资讯',url:'src/img/menu1.png'},
+          {name:'新闻资讯',url:'img/menu1.png'},
+          {name:'新闻资讯',url:'../../img/menu1.png'},
+          {name:'新闻资讯',url:'../../img/menu1.png'},
+          {name:'新闻资讯',url:'../../img/menu1.png'},
+          {name:'新闻资讯',url:'../../img/menu1.png'}
+      ]
+    };
+  },
+  created() {
+    this.getBannerData();
+  },
+  methods: {
+    getBannerData() {
+      this.$http.get("http://www.lovegf.cn:8899/api/getlunbo").then(result => {
+        if (result.body.status === 0) {
+          this.bannerList = result.body.message;
+          console.log(this.bannerList);
+          // Toast('导入数据失败');
+        } else {
+          Toast("导入数据失败");
+        }
+      });
+    }
+  }
+};
 </script>
-<style>
+<style lang="less">
+.banner {
+  .banner-list {
+    width: 100%;
+    height: 200px;
+    .mint-swipe-item {
+      &:nth-child(1) {
+        background-color: blue;
+      }
+      &:nth-child(2) {
+        background-color: #ccc;
+      }
+      &:nth-child(3) {
+        background-color: skyblue;
+      }
+      img {
+        width: 100%;
+      }
+    }
+  }
+}
 
+.title {
+    .mui-grid-view.mui-grid-9 {
+        border:none;
+        background-color: #fff; 
+        text-align: center;
+        >li {
+            border:none;
+            background-color: #fff; 
+            img {
+                width: 60px;
+                height: 60px;
+                background-color: pink;
+                display: block;
+                border-radius: 50%  ;
+                margin: auto;
+            }
+        }
+    }
+}
 </style>
 
